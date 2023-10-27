6214d98e236aae9b939526679e058286ee60d5fd
Mohammed <84126236+M7Saad@users.noreply.github.com>
2023-10-28 00:29:35 +0300

    JS_5-exercise_2

---
+++ JS/my-app/src/huh.js
@@ -0,0 +1,10 @@
+export default function F({ }) {
+  return (
+    <div>
+      <p>heeeeeey</p>
+    </div>
+  );
+}
+//Pitfall
+//React components are regular JavaScript functions, but their names must start with a capital letter or they won’t work!
+//يقطع لغتهم اللي ما تقدر تسمي الفانكشن بأي حرف صغير
